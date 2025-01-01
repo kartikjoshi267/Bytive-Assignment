f8c8fe2e34d96f7b0bfe002a2b8ef3076ca40ef0
Kartik <kartikjoshi267@gmail.com>
2025-01-01 12:29:44 +0530

    remove server.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import "express-async-errors";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import process from "process";
+
+const PORT = process.env.PORT || 3000;
 
 import userRoutes from "./routes/user-routes.js";
 import tasksRoutes from "./routes/tasks-routes.js";
@@ -26,7 +29,6 @@ app.get("/", (req, res) => {
 app.use("/users", userRoutes);
 app.use("/tasks", tasksRoutes);
 
-
 app.use("*", (req, res) => {
     throw new NotFoundError("Route not found");
 });
@@ -42,6 +44,26 @@ app.use((err, req, res, next) => {
         error: "We are having some server issues. Please try again later.",
         statusCode: STATUS_CODES.INTERNAL_SERVER_ERROR,
     });
+});
+
+process
+    .on("unhandledRejection", (reason, promise) => {
+        console.error(reason, "Unhandled Promise Rejection in application");
+        logger.error(reason, "Unhandled Promise Rejection in application");
+    })
+    .on("uncaughtException", (err) => {
+        console.error(reason, "Unhandled Promise Rejection in application");
+        logger.error(err, "Uncaught Exception thrown in application");
+        process.exit(1);
+    });
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.error(error);
+        logger.error(error);
+        return;
+    }
+    logger.info(`Server running on port ${PORT}`);
 });
 
 export default app;
